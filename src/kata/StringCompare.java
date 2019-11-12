64f6b87a127a7673f813db7df88c3d277c0f37e0
52-28 <aymeric.feillault@gmail.com>
2019-11-12 15:14:09 +0100

    solution

--- src/kata/StringCompare.java
+++ src/kata/StringCompare.java
@@ -1,4 +1,15 @@
 public class StringCompare {
 
-    // TODO
+    public static int compare(String compareIt, String toCompare){
+	
+		int result = 0;
+		if (compareIt == null || toCompare == null || compareIt.length() != toCompare.length())
+			return -1;
+		for (int i = 0 ; i < compareIt.length(); i++){
+			if (compareIt.charAt(i) == toCompare.charAt(i))		
+				result++;
+		}
+
+		return result;
+	}
 }
--- src/test/StringCompareTest.java
+++ src/test/StringCompareTest.java
@@ -9,4 +9,38 @@ public class StringCompareTest {
     }
 
     // TODO
+    @Test
+    public void test1() throws Exception {
+        Assert.assertEquals(StringCompare.compare("a","a"),1);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        Assert.assertEquals(StringCompare.compare("a","b"),0);
+    }
+
+    @Test
+    public void testaaba() throws Exception {
+        Assert.assertEquals(StringCompare.compare("aa","ba"),1);
+    }
+
+    @Test
+    public void testnull() throws Exception {
+        Assert.assertEquals(StringCompare.compare(null,"a"),-1);
+    }
+
+    @Test
+    public void testnullnull() throws Exception {
+        Assert.assertEquals(StringCompare.compare(null,null),-1);
+    }
+
+    @Test
+    public void testcassis() throws Exception {
+        Assert.assertEquals(StringCompare.compare("cassis","castor"),3);
+    }
+
+    @Test
+    public void testLengthNotEqual() throws Exception {
+        Assert.assertEquals(StringCompare.compare("tacos","poulpe"),-1);
+    }
 }
