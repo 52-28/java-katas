578a9f7c78eeca3281d34ba1188fc184e6507320
52-28 <aymeric.feillault@gmail.com>
2019-11-12 15:33:09 +0100

    solution

--- src/kata/StringCompare.java
+++ src/kata/StringCompare.java
@@ -1,14 +1,13 @@
 public class StringCompare {
 
-    public static int compare(String compareIt, String toCompare){
-	
+
+    public static int compare(String compareIt, String toCompare){	
 		int result = 0;
 		if (compareIt == null || toCompare == null || compareIt.length() != toCompare.length())
 			return -1;
-		for (int i = 0 ; i < compareIt.length(); i++){
+		for (int i = 0; i < compareIt.length(); i++)
 			if (compareIt.charAt(i) == toCompare.charAt(i))		
 				result++;
-		}
 
 		return result;
 	}
